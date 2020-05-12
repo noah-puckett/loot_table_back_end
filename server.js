e1df9d6123f71f3951dccc2c6889fb1ac9d2c145
Noah Puckett <noah.ingram.puckett@gmail.com>
2020-05-11 23:13:14 -0700

    still getting unhandled promise rejection warning

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
 require('dotenv').config();
+//require('./lib/utils/pg-pool')()  --GO FIGURE IT OUT
+// const express = require('express');
+// const cors = require('cors');
+// const pg = require('pg');
 
-const client = require('./lib/client');
+// // Database Client
+// const Client = pg.Client;
+// const client = new Client(process.env.DATABASE_URL);
 
-// Initiate database connection
-client.connect();
+// // Initialize DB connection
+// client.connect();
 
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 8080;
+// app.use(cors()); // enable CORS request
+// app.use(express.json()); // enable reading incoming json data
 
-// app.get('/animals', async(req, res) => {
-//   const data = await client.query('SELECT * from animals');
 
-//   res.json(data.rows);
-// });
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`CONGRATULATIONS! You're up an running on PORT ${PORT}`);
+	// eslint-disable-next-line no-console
+	console.log(`CONGRATULATIONS! You're up an running on PORT ${PORT}`);
 });
 
-module.exports = app;
+
+// app.get('/api/v1/loot', async(req, res) => {
+// 	try {
+// 		const loot = await client.query(`
+// 			SELECT * FROM loot;
+//         `);
+// 		res.json(loot.rows);
+// 	}
+// 	catch(err) {
+// 		console.log(err);
+// 		res.status(500).json({
+// 			error: err.message || err
+// 		});
+// 	}
+
+// });
+
